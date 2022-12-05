4f67839025d18c3e37230d8067699160c77e164b
sayli <sayliborole29@gmail.com>
2022-12-05 14:37:05 +0530

    update commit Sayli

--- Display.py
+++ Display.py
@@ -1,6 +1,19 @@
 # here the gui
-import Contacts as contacts
-
+# import Contacts as contactop
+import tkinter as tk
+import json
 # calling the function in Contacts.py
+main=tk.Tk()
+# contactop.PrintList()
+# w = tk.Label(main, text='GeeksForGeeks.org!')
 
-contacts.PrintList()
+file = open("ContactList.txt", "r")
+for line in file.readlines():
+    contact = json.loads(str(line))
+
+    # the .loads converts the string to dictionary
+    w = tk.Label(main, text=contact["Name"])
+    # print(contact["Name"])
+    w.pack()
+
+main.mainloop()
